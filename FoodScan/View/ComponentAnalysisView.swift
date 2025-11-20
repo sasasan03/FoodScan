1f864bd0c8bf7762f6cb2a93c2af27a0d587fb36
sasasan03 <sako.hiro62@gmail.com>
2025-11-21 00:10:32 +0900

    新しい画面を追加

---
+++ FoodScan/View/ComponentAnalysisView.swift
@@ -0,0 +1,18 @@
+//
+//  ComponentAnalysisView.swift
+//  FoodScan
+//
+//  Created by sako0602 on 2025/11/20.
+//
+
+import SwiftUI
+
+struct ComponentAnalysisView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ComponentAnalysisView()
+}

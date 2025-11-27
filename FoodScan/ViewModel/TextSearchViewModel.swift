7d30c0340b631df174095b2ca0afffeb287ee4b2
sasasan03 <sako.hiro62@gmail.com>
2025-11-27 22:32:34 +0900

    TextSearchViewModelでの検索の実装

--- FoodScan/ViewModel/TextSearchViewModel.swift
+++ FoodScan/ViewModel/TextSearchViewModel.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+import Combine
+
+@MainActor
+final class TextSearchViewModel: ObservableObject {
+    private var cancellable = Set<AnyCancellable>()
+    private let client = OpenFoodFactsApiClient()
+    @Published var foods: [OpenFoodFactsProduct]?
+    @Published var errorMessage: String?
+    
+    func searchFood(_ name: String) {
+        Task {
+            do {
+                let result = try await client.searchFood(itemName: name)
+                result
+                    .receive(on: DispatchQueue.main) 
+                    .sink(receiveCompletion: { completed in
+                    switch completed {
+                    case .finished:
+                        print("completed finished")
+                    case .failure(let failure):
+                        self.errorMessage = self.message(from: failure)
+                    }
+                }, receiveValue: { value in
+                    self.foods = value
+                }).store(in: &cancellable)
+            } catch {
+                errorMessage = message(from: error)
+            }
+        }
+    }
+    
+    private func message(from error: Error) -> String {
+        return "\(error)"
+    }
+}
